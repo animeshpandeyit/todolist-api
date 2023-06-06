bae2833bca94982ff76811176a48e0749ba7bcea
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2023-06-06 21:37:46 +0530

    user: req.user,

--- controllers/plant.js
+++ controllers/plant.js
@@ -1,42 +1,27 @@
 // import { Plant } from "../models/plant.js";
 
 // export const getplantData = async (req, res) => {
-//   const flowersData = req.plant._id;
-//   console.log(flowersData);
-//   const plant = await Plant.find({ plant: flowersData });
-//   res.status(200).send({
-//     success: true,
-//     message: "Product successfully found",
-//     plant,
-//   });
+//   try {
+//     const { _id } = req.plant;
+//     const plant = await Plant.findById(_id);
+
+//     if (!plant) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Plant not found",
+//       });
+//     }
+
+//     res.status(200).json({
+//       success: true,
+//       message: "Plant successfully found",
+//       plant,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       success: false,
+//       message: "Server error",
+//     });
+//   }
 // };
-
-
-
-import Plant from '../models/Plant';
-
-export const getPlantData = async (req, res) => {
-  try {
-    const { _id } = req.plant;
-    const plant = await Plant.findById(_id);
-    
-    if (!plant) {
-      return res.status(404).json({
-        success: false,
-        message: "Plant not found"
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      message: "Plant successfully found",
-      plant
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: "Server error"
-    });
-  }
-};
--- routes/plant.js
+++ routes/plant.js
@@ -1,10 +1,12 @@
 import express from "express";
 import multer from "multer";
-// import { Plant } from "../models/plant.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.js";
 
+import { Plant } from "../models/plant.js";
 // import { getplantData } from "../controllers/plant.js";
-
-const plantrouter = express.Router();
+const router = express.Router();
+// import { isAuthenticated } from "../middlewares/auth.js";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -18,31 +20,42 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-plantrouter.post("/profile", upload.single("image"), async (req, res, next) => {
-  try {
-    // Create a new image document
-    const newPlant = new Plant({
-      name: req.body.name,
-      description: req.body.description,
-      typesofflowers: {
-        category: req.body.category,
-        flowers: req.body.flowers,
-      },
-      image: {
-        data: req.file.filename,
-        contentType: req.file.mimetype,
-      },
-    });
-    // Save the plant to the database
-    await newPlant.save();
-
-    res.status(201).json({ message: "Plant uploaded successfully" });
-  } catch (err) {
-    console.error("Failed to upload plant:", err);
-    res.status(500).json({ error: "Failed to upload plant" });
+router.post(
+  "/profile",
+  // isAuthenticated,
+  upload.single("image"),
+  async (req, res, next) => {
+    try {
+      const { token } = req.cookies;
+      if (!token) {
+        res.status(401).json({ success: false, message: "login first!!" });
+      }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded._id);
+      // Create a new image document
+      const newPlant = new Plant({
+        name: req.body.name,
+        description: req.body.description,
+        typesofflowers: {
+          category: req.body.category,
+          flowers: req.body.flowers,
+        },
+        image: {
+          data: req.file.filename,
+          contentType: req.file.mimetype,
+        },
+        user: req.user,
+      });
+      // Save the plant to the database
+      await newPlant.save();
+      res.status(201).json({ message: "Plant uploaded successfully" });
+    } catch (err) {
+      console.error("Failed to upload plant:", err);
+      res.status(500).json({ error: "Failed to upload plant" });
+    }
   }
-});
+);
 
-// plantrouter.get("/getplantData", getplantData);
+// router.get("/getplantData", getplantData);
 
-export default plantrouter;
+export default router;
